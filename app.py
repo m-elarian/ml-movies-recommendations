0c919fa23e2a8668b7c843d05e8eb10808272454
m-elarian <61803779+m-elarian@users.noreply.github.com>
2020-05-05 04:59:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def home():
    #return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
    return populrMoviesLine
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+   app.run(debug=True)
